8d77120bdd3a479a7f05978db5846dff32b46eb5
ltargett <ltargett@jaguarlandrover.com>
2022-01-26 15:03:47 +0000

    Adding tutor view to URLs

--- LectureBoard/urls.py
+++ LectureBoard/urls.py
@@ -9,4 +9,13 @@ urlpatterns = [
     path('<str:req_Module_Code>/', views.ModuleBoardView, name='ModuleBoard'),
     # Example: /LectureBoard/WM393/1, this is for viewing an invidual lecture day
     path('<str:req_Module_Code>/<int:lecture_id>/', views.LectureDayView, name='LectureDay')
+
+    # ADMIN:
+    # Example: LectureBoard/Tutor/, this is where a lecturer can view/edit/delete all their modules
+    path('', views.LectureBoardView, name='LectureBoardTutor'), 
+    # Example: LectureBoard/Tutor/WM393/, this is to view/edit/delete an invidual Module Board
+    path('Tutor/<str:req_Module_Code>/', views.ModuleBoardView, name='ModuleBoardTutor'),
+    # Example: LectureBoard/Tutor/WM393/1, this is to view/edit/delete an invidual lecture day
+    path('Tutor/<str:req_Module_Code>/<int:lecture_id>/', views.LectureDayView, name='LectureDayTutor')
+
 ]
