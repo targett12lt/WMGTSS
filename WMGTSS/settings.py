b900b5f1526af5782cdb507e7d05461fcd399c3e
ltargett <ltargett@jaguarlandrover.com>
2022-02-07 14:14:00 +0000

    Adding LectureBoard static files dir to STATICFILES_DIRS

--- WMGTSS/settings.py
+++ WMGTSS/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'LectureBoard', 'static')  # Adding LectureBoard static files
 )
 
 # Default primary key field type
